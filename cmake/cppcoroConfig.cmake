39a5588618e4d8d8e40af5e501b39c63bc033a6b
Octavian Cacina <octavian.cacina@outlook.com>
2020-12-03 20:59:36 +0100

    Set C++ standard to 20 when checking for coroutines support

--- cmake/cppcoroConfig.cmake
+++ cmake/cppcoroConfig.cmake
@@ -1,6 +1,8 @@
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 include(CMakeFindDependencyMacro)
+
+set(CMAKE_CXX_STANDARD 20)
 find_dependency(Coroutines QUIET REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/cppcoroTargets.cmake")
